818dc1f35161e653268be2389e4eaec0c00baaa8
Alécio <alecio.godoi@ymail.com>
2021-09-01 11:54:52 -0300

    Aula 06

--- Aula 06 - 01-09-2021/src/Heranca_01/Gerente.java
+++ Aula 06 - 01-09-2021/src/Heranca_01/Gerente.java
@@ -1,14 +1,16 @@
 package Heranca_01;
 
 public class Gerente extends Funcionario {
-	int senha;
+	private int senha;
 
 	public Gerente() {
 		super();   // chamada do método construtor da classe mãe
 	}
 
-	public Gerente(String nome, String cpf, double salario) {
+	public Gerente(String nome, String cpf, double salario, int pass) {
+		// chamada do método construtor da classe mãe
 		super(nome, cpf, salario);
+		this.senha = pass;
 	}
 
 	@Override
@@ -16,4 +18,11 @@ public class Gerente extends Funcionario {
 		return "Gerente [senha=" + senha + "] - " + super.toString();
 	}
 
+	public boolean autentica(int pass) {
+		if (senha == pass)
+			System.out.println("Autenticado!");
+		else 
+			System.out.println("Senha Inválida!");
+		return true;
+	}
 }
--- Aula 06 - 01-09-2021/src/Heranca_01/Main.java
+++ Aula 06 - 01-09-2021/src/Heranca_01/Main.java
@@ -8,12 +8,21 @@ public class Main {
 		funcionarios[3] = new Funcionario("Marcos", "777.875.891-99", 4217.52);
 		funcionarios[4] = new Funcionario("Virginia", "076.098.211-77", 4217.52);
 		
-		Gerente ger01 = new Gerente("José", "091.123.654-11", 10532.21 );
+		Gerente ger01 = new Gerente("José", "091.123.654-11", 10532.21, 1234 );
 		//ger01.setNome("José");
 		System.out.println(ger01);
+
+		ger01.setNome("");
+		ger01.autentica(12);
+		ger01.autentica(123);
+		ger01.autentica(1234);
 		
 		for (Funcionario func: funcionarios) {
 		    System.out.println(func);
+		}
+		
+		for (Funcionario func: funcionarios) {
+		    System.out.println(func.getNome());
 		}
 	}
 }
