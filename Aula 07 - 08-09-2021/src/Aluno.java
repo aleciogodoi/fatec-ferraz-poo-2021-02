ece1c9521733b97e5f0ae14b71e29587b2dc0105
Alécio <alecio.godoi@ymail.com>
2021-09-08 12:33:03 -0300

    Aula 07

--- Aula 07 - 08-09-2021/src/Aluno.java
+++ Aula 07 - 08-09-2021/src/Aluno.java
@@ -1,4 +1,31 @@
-
-public class Aluno {
-
+public class Aluno extends Pessoa{
+	//Atributos
+	private String ra;
+	private int nrFaltas;
+	// Construtores
+	public Aluno() {
+	}
+	public Aluno(String nome, String identidade, Data dtNascimento,
+				 String ra, int nrFaltas) {
+		super(nome, identidade, dtNascimento);
+		this.ra = ra;
+		this.nrFaltas = nrFaltas;
+	}
+	// Getters e Setters
+	public String getRa() {
+		return ra;
+	}
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+	public int getNrFaltas() {
+		return nrFaltas;
+	}
+	public void setNrFaltas(int nrFaltas) {
+		this.nrFaltas = nrFaltas;
+	}
+	@Override
+	public String toString() {
+		return "Aluno [ra=" + ra + ", nrFaltas=" + nrFaltas + "] - " + super.toString();
+	}
 }
--- Aula 07 - 08-09-2021/src/Main.java
+++ Aula 07 - 08-09-2021/src/Main.java
@@ -10,5 +10,8 @@ public class Main {
 		
 		Professor prof01 = new Professor("Ana","3100987-09",new Data(20, 07, 1990),"1010-10");
 		System.out.println(prof01);
+		
+		Aluno aluno01 = new Aluno("Marilia","09876543-98",new Data(07,05,1998), "203040-77", 5);
+		System.out.println(aluno01);
 	}
 }
