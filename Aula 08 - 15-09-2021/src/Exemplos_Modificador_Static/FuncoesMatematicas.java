f914e566a0a4d1f39aa3706c6e856919f1aefcc6
Alécio <alecio.godoi@ymail.com>
2021-09-15 09:58:23 -0300

    Aula 08

---
+++ Aula 08 - 15-09-2021/src/Exemplos_Modificador_Static/FuncoesMatematicas.java
@@ -0,0 +1,21 @@
+package Exemplos_Modificador_Static;
+
+public class FuncoesMatematicas {
+	
+	public static int exponencial(int x, int y) {
+		if (y == 0)
+			return 1;
+		return x * exponencial(x, y - 1);
+	}
+
+	public static int fatorial(int x) {
+		if (x == 0)
+			return 1;
+		return x * fatorial(x - 1);
+	}
+	
+	public static int soma(int x, int y) {
+		return x + y;
+	}
+	
+}
---
+++ Aula 08 - 15-09-2021/src/Exemplos_Modificador_Static/Main.java
@@ -0,0 +1,13 @@
+package Exemplos_Modificador_Static;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(FuncoesMatematicas.exponencial(5, 2));
+		System.out.println(FuncoesMatematicas.fatorial(5));
+		System.out.println(FuncoesMatematicas.soma(5, 7));
+
+	}
+
+}
