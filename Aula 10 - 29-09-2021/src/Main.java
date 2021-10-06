3127effd9e43b5e9a3b0e9db664a8aaafef86c44
Alécio <alecio.godoi@ymail.com>
2021-10-06 10:13:55 -0300

    Aula 11 - Despesas

--- Aula 10 - 29-09-2021/src/Main.java
+++ Aula 10 - 29-09-2021/src/Main.java
@@ -8,13 +8,11 @@ public class Main {
 	private static String itemDespesa;
 	private static double valor;
 	private static DespesaPessoal despesa;
-	private Scanner meuScanner = new Scanner(System.in); 
-	Main(){
-		
+	private static Scanner meuScanner = new Scanner(System.in); 
+	Main(){		
 		System.out.print("CPF: ");
 		String cpf = meuScanner.next();
 		despesa = new DespesaPessoal(cpf);
-
 	}
 	
 	public static void main(String[] args) {	
@@ -38,15 +36,60 @@ public class Main {
 			System.out.println(menu);
 			System.out.print("Opção: ");
 			opcao = meuScanner.nextInt();
-		} while(opcao != 8);
+			switch (opcao) {
+			case 1:
+				lancamento();
+				break;
+			case 2:
+				totalGeral();
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				despesa.imprime();
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			}
+		} while(opcao != 0);
 		meuScanner.close();
 		System.out.println("F I M");
 	}
 
-	public void entradaDia() {	
+	public static void lancamento() {
+		System.out.println();
+		entradaDia();
+		entradaMes();
+		entradaAno();
+		meuScanner.nextLine();
+		System.out.print("Despesa: ");
+		itemDespesa = meuScanner.nextLine();
+		System.out.print("Valor: ");
+		valor = meuScanner.nextDouble();
+		despesa.getDespesas().add(new Despesa(LocalDate.of(ano, mes, dia), itemDespesa, valor ));
 	}
-	public void entradaMes() {	
+	
+	public static void totalGeral() {
+		System.out.println("\n-----------------------------------------------------");
+	    System.out.println("Despesas - CPF: "+despesa.getCpf());
+		System.out.println("-----------------------------------------------------");
+		System.out.printf("Total Geral: %.2f",despesa.getTotal());
+		System.out.println("\n-----------------------------------------------------");
 	}
-	public void entradaAno() {	
+	public static void entradaDia() {	
+		System.out.print("Dia: ");
+		dia = meuScanner.nextInt();
+	}
+	public static void entradaMes() {	
+		System.out.print("Mês: ");
+		mes = meuScanner.nextInt();
+	}
+	public static void entradaAno() {
+		System.out.print("Ano: ");
+		ano = meuScanner.nextInt();
 	}
 }
