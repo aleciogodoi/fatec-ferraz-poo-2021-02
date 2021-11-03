e111d734917d0d96a730752409e62a299234aa88
Alécio <alecio.godoi@ymail.com>
2021-11-03 12:24:53 -0300

    Aula 14

---
+++ Aula 14-03-11-2021/src/agenda/Agenda.java
@@ -0,0 +1,47 @@
+package agenda;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Agenda {
+	private ArrayList<Contato> contatos;
+	
+	Agenda() {
+		contatos = new ArrayList<Contato>();
+	}
+	
+	public void adicionar() {
+		Contato contato = entrada();
+		contatos.add(contato);
+		// gravar(contato.toString());
+	}
+	
+	public void listar() {
+		System.out.println();
+		for (Contato contato: this.getContatos())
+			System.out.print(contato.toString());
+		System.out.println();
+	}
+	
+	public Contato entrada() {
+		Contato contato = new Contato();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nNome: ");
+		String nome = sc.nextLine();
+
+		System.out.print("Telefone: ");
+		String telefone = sc.nextLine();
+
+		contato.setNome(nome);
+		contato.setTelefone(telefone);
+		
+		System.out.println();
+		return contato;
+	}
+	
+	public ArrayList<Contato> getContatos(){
+		return contatos;
+	}
+	
+}
