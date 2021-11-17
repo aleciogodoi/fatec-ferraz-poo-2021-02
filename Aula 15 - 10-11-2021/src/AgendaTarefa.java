cf5a75de203c2d7c494da2fac81eb493fbfae2de
Alécio <alecio.godoi@ymail.com>
2021-11-17 11:07:26 -0300

    Aula 16

---
+++ Aula 15 - 10-11-2021/src/AgendaTarefa.java
@@ -0,0 +1,143 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AgendaTarefa {
+	private ArrayList<Tarefa> tarefas;
+	
+	AgendaTarefa() {
+		tarefas = new ArrayList<Tarefa>();
+		this.carga();	
+	}
+	
+	public ArrayList<Tarefa> carga() {
+		ArrayList<String> dados = Arquivo.ler();
+		for (String dado: dados) {
+			Tarefa tarefa = new Tarefa();
+			String[] aux = dado.split(",");
+			
+			tarefa.setId(Integer.valueOf(aux[0]));
+			
+			if (aux[1].equalsIgnoreCase("null"))
+				tarefa.setNome(null);
+			else
+				tarefa.setNome(aux[1]);
+			
+			if (aux[2].equalsIgnoreCase("null"))
+				tarefa.setDescricao(null);
+			else
+				tarefa.setDescricao(aux[2]);
+				
+			
+			// Tratamento para as datas 2021-11-10
+			
+			if (aux[3].equalsIgnoreCase("null")) 
+				tarefa.setDtInicio(null);
+			else {
+				String[] dataIni = aux[3].split("-");
+	
+				tarefa.setDtInicio(
+						LocalDate.of(
+								Integer.valueOf(dataIni[0]),
+								Integer.valueOf(dataIni[1]),
+								Integer.valueOf(dataIni[2])
+						)
+				);
+			}
+			
+			if (aux[4].equalsIgnoreCase("null")) 
+				tarefa.setDtFim(null);
+			else {
+				String[] dataFim = aux[4].split("-");
+	
+				tarefa.setDtFim(
+						LocalDate.of(
+								Integer.valueOf(dataFim[0]),
+								Integer.valueOf(dataFim[1]),
+								Integer.valueOf(dataFim[2])
+						)
+				);
+			}
+			
+			if (aux[5].equalsIgnoreCase("null")) 
+				tarefa.setStatus(null);
+			else
+				tarefa.setStatus(aux[5]);
+			
+			tarefas.add(tarefa);
+			
+		}
+		return tarefas;
+	}
+	
+	public Tarefa consultar() {
+		Tarefa tarefa = null;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\nNome: ");
+		String nome = sc.nextLine();
+		for (Tarefa t: this.getTarefas()) {
+			if (t.getNome().equalsIgnoreCase(nome))
+				tarefa = t;
+		}
+		return tarefa;
+	}
+
+	public void adicionar() {
+		Tarefa tarefa = entrada();
+		tarefas.add(tarefa);
+		Arquivo.gravar(tarefa.toString());
+	}
+	
+	public void listar() {
+		System.out.println();
+		for (Tarefa tarefa: this.getTarefas()) 
+			System.out.print(tarefa.toString());
+		System.out.println();
+	}
+	
+	public Tarefa entrada() {
+		Tarefa tarefa = new Tarefa();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nNome: ");
+		tarefa.setNome(sc.nextLine());
+
+		System.out.print("Descricao: ");
+		tarefa.setDescricao(sc.nextLine());
+
+		System.out.print("Dia Inicio: ");
+		int diaIni = sc.nextInt();
+
+		System.out.print("Mes Inicio: ");
+		int mesIni = sc.nextInt();
+
+		System.out.print("Ano Inicio: ");
+		int anoIni = sc.nextInt();
+		tarefa.setDtInicio(LocalDate.of(anoIni, mesIni, diaIni));
+		
+		System.out.print("Dia Fim: ");
+		int diaFim = sc.nextInt();
+
+		System.out.print("Mes Fim: ");
+		int mesFim = sc.nextInt();
+
+		System.out.print("Ano Fim: ");
+		int anoFim = sc.nextInt();
+		
+		if (diaFim==0) 
+			tarefa.setDtFim(null);
+		else
+			tarefa.setDtFim(LocalDate.of(anoFim, mesFim, diaFim));
+		
+		sc.nextLine();
+		System.out.print("Status: ");
+		tarefa.setStatus(sc.nextLine());
+		
+		System.out.println();
+		return tarefa;
+	}
+	
+	public ArrayList<Tarefa> getTarefas() {
+		return tarefas;
+	}
+}
--- Aula 15 - 10-11-2021/src/Main.java
+++ Aula 15 - 10-11-2021/src/Main.java
@@ -1,15 +1,19 @@
 import java.time.LocalDate;
 import java.util.Scanner;
 
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		int opcao = 0;
+		AgendaTarefa agenda = new AgendaTarefa();
 		
 		do {
 			opcao = menu();
 			switch (opcao) {
 			case 1:
+				agenda.adicionar();
 				break;
 			case 2:
 				break;
@@ -28,7 +32,7 @@ public class Main {
 					+ "2 - Consultar\n"
 					+ "3 - Listar\n"
 					+ "0 - Sair\n"
-					+ "Opção: ";
+					+ "Opcao: ";
 		
 		System.out.print(menu);
 		opcao = scan.nextInt();
